3d32eff3aa621b6130976f372dc3508a8c30e36b
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2024-01-13 02:21:10 +0530

    useState

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+ function App() {
+
+  const [count, setCount] = useState(4)
+
+  function decrementcount() {
+    setCount(prevCount => prevCount -1)
+   // setCount(prevCount => prevCount -1)
+  }
+
+  function incrementcount() {
+    setCount(prevCount => prevCount + 1)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <button onClick={decrementcount}>-</button>
+      <span>{count}</span>
+      <button onClick={incrementcount} >+</button>
+    </>
   );
 }
 
