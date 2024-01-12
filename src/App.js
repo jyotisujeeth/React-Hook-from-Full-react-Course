7fe6b9c6cd2b55a09bdcfccf2bad2e7fa7ba584c
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2024-01-13 04:59:03 +0530

    use efeectet en2

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
 ////////////////////////useEffect example 2/////////////////////////////////////useEffect example 1 ////////////////
+////////////////////re size ing the window width //////////////
 import React, { useState, useEffect } from 'react';
 
 export default function App() {
-     const [resourceType, setResourceType] = useState("Posts");
-    const [items, setItems] = useState([])
-      console.log("render");
-    
-     useEffect(() => {
-       fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
-         .then((response) => response.json())
-         .then((json) => setItems(json))
-         .then((json) => console.log(json));
-        // console.log('render the data');
-     }, [resourceType])
+     const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+     
+     const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+     }
+
+
+    useEffect(() => {
+      window.addEventListener('resize', handleResize)
+
+      return () => {
+        window.removeEventListener('resize', handleResize)
+      }
+    }, [])
     
     return (
       <>
-        <div>
-          <button onClick={() => setResourceType("posts")}>Posts</button>
-          <button onClick={() => setResourceType("users")}>Users</button>
-          <button onClick={() => setResourceType("comments")}>Comments</button>
-        </div>
-        <h1>{resourceType}</h1>
-        {items.map(item => {
-            return <pre>{JSON.stringify(items)}</pre>
-        })
-         }
+        <div>{windowWidth}</div>
+        
       </>
     );
         };
