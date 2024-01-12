a3f2f1faadebb18ce4c5ccd641016f43a7b939b2
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2024-01-13 04:40:35 +0530

    use efeectet en

--- src/App.js
+++ src/App.js
@@ -1,1 +1,31 @@
-/////////////////useEfeect////////////////
+////////////////////////useEffect example 2/////////////////////////////////////useEffect example 1 ////////////////
+import React, { useState, useEffect } from 'react';
+
+export default function App() {
+     const [resourceType, setResourceType] = useState("Posts");
+    const [items, setItems] = useState([])
+      console.log("render");
+    
+     useEffect(() => {
+       fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+         .then((response) => response.json())
+         .then((json) => setItems(json))
+         .then((json) => console.log(json));
+        // console.log('render the data');
+     }, [resourceType])
+    
+    return (
+      <>
+        <div>
+          <button onClick={() => setResourceType("posts")}>Posts</button>
+          <button onClick={() => setResourceType("users")}>Users</button>
+          <button onClick={() => setResourceType("comments")}>Comments</button>
+        </div>
+        <h1>{resourceType}</h1>
+        {items.map(item => {
+            return <pre>{JSON.stringify(items)}</pre>
+        })
+         }
+      </>
+    );
+        };
