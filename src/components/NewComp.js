1970360c6fa7c74df99114fe97335e68ec17d36b
jyotisujeeth <105577483+jyotisujeeth@users.noreply.github.com>
2024-01-19 13:39:20 +0530

    added img

--- src/components/NewComp.js
+++ src/components/NewComp.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-
+// import bell from "./bell.jpg"
+// import not from "./not.jpg";
 export class NewComp extends Component {
   constructor(props) {
     super(props);
@@ -7,18 +8,23 @@ export class NewComp extends Component {
     this.state = {
       message: "subscribe to my new channel",
       sub: "subscribe",
+      imageURL:
+        "https://th.bing.com/th?id=OIP.WKeWzEzMK_O7FkMS7C0m-AHaHe&w=248&h=251&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2",
     };
   }
 
   styles = {
     fontStyle: "italic",
     color: "red",
+    
   };
 
   ButtonChange=() => {
     this.setState({
-         message: "hit the bell icon to never mis an updtae ",
+      message: "hit the bell icon to never mis an updtae ",
       sub: "subscribed",
+      imageURL:
+        "https://th.bing.com/th?id=OIP.wTBYwLv7x8d1Hi9B3TdDWgHaH_&w=240&h=259&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2",
     });
   }
   render() {
@@ -26,6 +32,8 @@ export class NewComp extends Component {
       <div className="App">
         <h3 style={this.styles}>{this.state.message}</h3>
         <button onClick={this.ButtonChange}>{this.state.sub}</button>
+     <p/>
+     <img style={(width: "30px", height:"15px")} src={this.state.imageURL} alt='thebell iamge'/>
       </div>
     );
   }
